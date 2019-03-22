d7d6ae91c9b76cf3687565156179d63c6f1b5e2e
Benjamin Lund <lund.b92@gmail.com>
2019-03-22 15:46:18 +0100

    Added Mockdata for Program page + Program model + Program service to deserialize json to list

---
+++ GivskudApp/GivskudApp/Models/ProgramModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GivskudApp.Models
+{
+    public class ProgramModel
+    {
+        public double Time { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ GivskudApp/GivskudApp/Services/ProgramService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+using GivskudApp.Models;
+using Newtonsoft.Json;
+
+namespace GivskudApp.Services
+{
+    public class ProgramService
+    {
+        public IList<ProgramModel> Program { get; }
+
+        // ###
+        // In future iteration of the app this data should come from an api
+        // ###
+        public ProgramService() // Need to use Encoding to make it utf-8 (ø æ å)
+        {
+            var assembly = GetType().GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream("GivskudApp.MockData.ProgramMockData.json");
+
+            using (var reader = new StreamReader(stream))
+            {
+                var json = reader.ReadToEnd();
+                List<ProgramModel> list = JsonConvert.DeserializeObject<List<ProgramModel>>(json);
+
+                Program = new List<ProgramModel>(list.OrderBy(n => n.Title));
+            }
+        }
+    }
+}
