b85d16848dc53e212c42bae35bf974509e9807fe
Benjamin Lund <lund.b92@gmail.com>
2019-03-22 15:20:49 +0100

    Added navigation drawer + navigation functionality + Global styles for navigation and pages

--- GivskudApp/GivskudApp/MainPage.xaml.cs
+++ GivskudApp/GivskudApp/MainPage.xaml.cs
@@ -4,14 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using GivskudApp.Models;
+using GivskudApp.Views;
 
 namespace GivskudApp
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : MasterDetailPage
     {
+        public List<NavigationModel> MenuList { get; set; }
+
         public MainPage()
         {
             InitializeComponent();
+
+            MenuList = new List<NavigationModel>
+            {
+                // Adding menu items to menuList and you can define title ,page and icon
+                new NavigationModel() { Title = "Forside", Icon = "home.png", TargetType = typeof(HomePage) },
+                new NavigationModel() { Title = "Spil", Icon = "game.png", TargetType = typeof(HomePage) },
+                // Change these to right pages
+                new NavigationModel() { Title = "Nyheder", Icon = "news.png", TargetType = typeof(HomePage) },
+                new NavigationModel() { Title = "Årskort", Icon = "seasonpass.png", TargetType = typeof(HomePage) },
+                new NavigationModel() { Title = "Dyr", Icon = "animals.png", TargetType = typeof(HomePage) },
+                new NavigationModel() { Title = "Kontakt Os", Icon = "contact.png", TargetType = typeof(HomePage) },
+                new NavigationModel() { Title = "Køb Billetter", Icon = "tickets.png", TargetType = typeof(HomePage) },
+            };
+
+            // Setting our list to be ItemSource for ListView in MainPage.xaml
+            menuItemList.ItemsSource = MenuList;
+
+            // Initial navigation, set to the home page
+            menuItemList.SelectedItem = MenuList[0];
+        }
+
+        // Event for Menu Item selection, here we are going to handle navigation based
+        // on user selection in menu ListView
+        private void OnMenuItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = (NavigationModel)e.SelectedItem;
+            Type page = item.TargetType;
+
+
+            Detail = new NavigationPage((Page)Activator.CreateInstance(page));
+            IsPresented = false;
         }
     }
 }
---
+++ GivskudApp/GivskudApp/Models/NavigationModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GivskudApp.Models
+{
+    public class NavigationModel
+    {
+        public string Title { get; set; }
+        public string Icon { get; set; }
+        public Type TargetType { get; set; }
+    }
+}
