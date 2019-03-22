1af8e4685acb1db7d46b55c7adcfc2c5cd11ad40
Benjamin Lund <lund.b92@gmail.com>
2019-03-22 16:09:04 +0100

    Added mock data for news + news model + news service to parse the json data to list

---
+++ GivskudApp/GivskudApp/Models/NewsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GivskudApp.Models
+{
+    public class NewsModel
+    {
+        public string Title { get; set; }
+        public string BodyText { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ GivskudApp/GivskudApp/Services/NewsService.cs
@@ -0,0 +1,33 @@
+﻿using GivskudApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace GivskudApp.Services
+{
+    public class NewsService
+    {
+        public IList<NewsModel> News { get; }
+
+        // ###
+        // In future iteration of the app this data should come from an api
+        // ###
+        public NewsService() // Need to use Encoding to make it utf-8 (ø æ å)
+        {
+            var assembly = GetType().GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream("GivskudApp.MockData.NewsMockData.json");
+
+            using (var reader = new StreamReader(stream))
+            {
+                var json = reader.ReadToEnd();
+                List<NewsModel> list = JsonConvert.DeserializeObject<List<NewsModel>>(json);
+
+                News = new List<NewsModel>(list.OrderBy(n => n.PublishDate));
+            }
+        }
+    }
+}
