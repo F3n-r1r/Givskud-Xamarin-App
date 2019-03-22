6f09f3d440a28bbfc6d2556648f073a665002676
Benjamin Lund <lund.b92@gmail.com>
2019-03-22 15:36:02 +0100

    Added design of home page + functionality for pushing navigation page when button is clicked + images

--- GivskudApp/GivskudApp/Views/HomePage.xaml.cs
+++ GivskudApp/GivskudApp/Views/HomePage.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,5 +15,26 @@ namespace GivskudApp.Views
 		{
 			InitializeComponent ();
 		}
-	}
+
+        // Handle button click -> Push new page
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            var btn = (Button)sender;
+            var id = btn.ClassId;
+
+            if (id == "TourPageBtn")
+            {
+                await Navigation.PushAsync(new GuidePage());
+            }
+            else if (id == "MapPageBtn")
+            {
+                await Navigation.PushAsync(new MapPage());
+            }
+            else if (id == "ProgramPageBtn")
+            {
+                await Navigation.PushAsync(new ProgramPage());
+            }
+
+        }
+    }
 }
