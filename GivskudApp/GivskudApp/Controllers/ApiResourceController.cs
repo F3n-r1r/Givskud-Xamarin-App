f88ec9deaa35d227af053204a3f67a6eee21ffe0
jiri-kralovec-eal <jiri0042@edu.eal.dk>
2019-04-01 19:09:17 +0200

    Updates API Resource Controller
    Updates API Resource Controller

--- GivskudApp/GivskudApp/Controllers/ApiResourceController.cs
+++ GivskudApp/GivskudApp/Controllers/ApiResourceController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -10,6 +11,92 @@ using GivskudApp.Controllers;
 using System.Diagnostics;
 
 namespace GivskudApp.ResourceControllers {
+
+    class ApiResource {
+
+        private string AuthenticationToken = "C4oILgIT7dTqLye9LJZ0Hr9Xedp7RleQAxw5NVHE";
+        private string ApiBaseUri = "";
+
+        public string Get(string Endpoint, Dictionary<string,string> Headers = null) {
+
+            return GetAsync(Endpoint, Headers).Result;
+
+        }
+
+        private async Task<string> GetAsync(string Route, Dictionary<string,string> Headers) {
+
+            using(HttpClient Client = GetClient(Headers)) {
+
+                string Result = null;
+
+                try {
+
+                    HttpResponseMessage HttpRequest = await Client.GetAsync(ApiBaseUri + Route);
+                    
+                    if(HttpRequest.StatusCode == HttpStatusCode.OK) {
+                        
+                        if(HttpRequest.Content != null) {
+                            HttpContentHeaders ContentHeaders = HttpRequest.Content.Headers;
+                            if(ContentHeaders.ContentType.MediaType == "application/json") {
+
+                                Result = await HttpRequest.Content.ReadAsStringAsync();
+                                Console.WriteLine("Test success");
+
+                            } else {
+                                Console.WriteLine("Test failure: Response body is expected to be in 'application/json' format.");
+                            }
+                        } else {
+                            Console.WriteLine("Test failure: Request content is null");
+                        }
+
+                    } else {
+                        switch(HttpRequest.StatusCode) {
+                            case HttpStatusCode.Unauthorized:
+                                Console.WriteLine("Test failure: Unauthorized");
+                                break;
+                            case HttpStatusCode.NotFound:
+                                Console.WriteLine("Test failure: Resource not found");
+                                break;
+                            default:
+                                Console.WriteLine("Test failure: Status code");
+                                break;
+                        }
+                    }
+
+                } catch (Exception e) {
+                    Console.WriteLine("Unhandled exception: {0}", e.Message);
+                }
+
+                return Result;
+
+            }
+
+        }
+
+        private HttpClient GetClient(Dictionary<string, string> Headers = null) {
+
+            HttpClient Client = new HttpClient();
+
+            // Charset header
+            Client.DefaultRequestHeaders.AcceptCharset.Add(new StringWithQualityHeaderValue("utf-8"));
+
+            // Authentication
+            Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", AuthenticationToken);
+
+            // Additional headers
+            if(Headers != null && Headers.Count > 0) {
+                foreach(KeyValuePair<string,string> Entry in Headers) {
+                    Client.DefaultRequestHeaders.Add(Entry.Key, Entry.Value);
+                }
+            }
+
+            return Client;
+
+        }
+
+    }
+
+    [Obsolete]
     class ApiResourceController {
 
         private string _Api = "https://givskudbackoffice20190327114413.azurewebsites.net/umbraco/api";
@@ -26,10 +113,9 @@ namespace GivskudApp.ResourceControllers {
         }
         public async Task<string> Get() {
             using(HttpClient _Client = GetClient()) {
-                _Client.Timeout = TimeSpan.FromSeconds(20);
                 try {
                     HttpResponseMessage Request = await _Client.GetAsync(_Api + _Endpoint).ConfigureAwait(false);
-                    if(Request.IsSuccessStatusCode) {
+                    if(Request.StatusCode == System.Net.HttpStatusCode.OK) {
                         return await Request.Content.ReadAsStringAsync();
                     } else {
                         PopupController.Message("Service error", "Please check your internet connection and try again. If the issue persists, contact our staff.", "OK");
