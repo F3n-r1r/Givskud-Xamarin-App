5cc14560b5188fa29dd42d3c692249475fcd4d83
jiri-kralovec-eal <jiri0042@edu.eal.dk>
2019-04-06 14:01:41 +0200

    SeasonPass now uses separate service like the rest of the pages

---
+++ GivskudApp/GivskudApp/Services/SeasonPassService.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+using Newtonsoft.Json;
+
+using Plugin.Settings;
+
+using GivskudApp.Models;
+using GivskudApp.Controllers;
+using GivskudApp.ResourceControllers;
+using GivskudApp.CryptographyService;
+using System.Globalization;
+
+namespace GivskudApp.Services {
+    class SeasonPassService {
+
+        public SeasonPassModel SeasonPass = null;
+        private string _SeasonPassDataKey = "applicationSeasonPassObject";
+        
+        public void Remove() {
+
+            Task.Run(() => {
+                SeasonPass = null;
+                CrossSettings.Current.Remove(_SeasonPassDataKey);
+            });
+            
+        }
+
+        public void Get(string input = null) {
+
+            if(input == null) {
+
+                string StoredValue = CrossSettings.Current.GetValueOrDefault(_SeasonPassDataKey, null);
+
+                if(StoredValue == null) {
+                    SeasonPass = null;
+                } else {
+                    SeasonPass = ReturnOrRefuse(StoredValue);
+                }
+
+            } else {
+
+                string inputEncrypted = EncDecService.Hash(input);
+                SeasonPass = ReturnOrRefuse(inputEncrypted);
+                    
+                if(SeasonPass != null) {
+                    CrossSettings.Current.AddOrUpdateValue(_SeasonPassDataKey, inputEncrypted);
+                }
+
+            }
+
+        }
+
+        private SeasonPassModel ReturnOrRefuse(string encrypted) {
+            
+            ApiResource ApiResource = new ApiResource();
+            Dictionary<string, string> ApiResourceHeaders = new Dictionary<string, string>();
+            ApiResourceHeaders.Add("PassID", encrypted);
+            string ApiResourceJson = ApiResource.Get("/seasonpass/get", ApiResourceHeaders);
+            
+            if(ApiResourceJson != null) {
+                try {
+                    List<SeasonPassModel> ResponseData = JsonConvert.DeserializeObject<List<SeasonPassModel>>(ApiResourceJson);
+                    if(ResponseData != null && ResponseData.Count > 0) {
+                        
+                        SeasonPassModel Temp = ResponseData[0];
+                        
+                        if(IsPassValid(Temp.ValidTo)) {
+                            
+                            Temp.Holder = EncDecService.Decrypt(Temp.Holder);
+
+                            CrossSettings.Current.AddOrUpdateValue(_SeasonPassDataKey, encrypted);
+
+                            return Temp;
+
+                        } else {
+                            return null;
+                        }
+                    } else {
+                        return null;
+                    }
+                } catch (Exception e) {
+                    return null;
+                }
+            } else {
+                return null;
+            }
+        }
+        
+        private bool IsPassValid(string ValidTo) {
+
+            DateTime To = DateTime.ParseExact(ValidTo, "dd-MM-yyyy", CultureInfo.InvariantCulture).Date;
+            DateTime Today = DateTime.Now.Date;
+
+            return To.CompareTo(Today) >= 0;
+
+        }
+
+    }
+}
--- GivskudApp/GivskudApp/ViewModel/SeasonPassViewModel.cs
+++ GivskudApp/GivskudApp/ViewModel/SeasonPassViewModel.cs
@@ -14,6 +14,7 @@ using Newtonsoft.Json;
 using Plugin.Settings;
 using Xamarin.Forms;
 using GivskudApp.Models;
+using GivskudApp.Services;
 using GivskudApp.Controllers;
 using GivskudApp.ResourceControllers;
 using GivskudApp.CryptographyService;
@@ -22,152 +23,58 @@ namespace GivskudApp.ViewModel
 {
     class SeasonPassViewModel : INotifyPropertyChanged {
 
-        private string _SeasonPassDataKey = "applicationSeasonPassObject";
-        public bool _IsBusy = false;
-        public bool _HasValidPass = false;
-        public bool _IsWorking = false;
-        public string _SeasonPassInput = "";
-        public SeasonPassModel _ValidPassData = null;
+        SeasonPassService Service = new SeasonPassService();
 
+        public string _UserInputValue = "";
+        public string UserInputValue { get { return _UserInputValue; } set { _UserInputValue = value; } }
+
+        public bool _IsBusy = true;
+        public bool IsBusy { get { return _IsBusy; } set { _IsBusy = value; } }
+
+        public SeasonPassModel Pass { get { return Service.SeasonPass; } }
+        public bool ShowPassDetails { get { return Pass != null && IsBusy == false; } }
+        public bool ShowPassInput { get { return Pass == null && IsBusy == false; } }
         public event PropertyChangedEventHandler PropertyChanged;
-
-        void OnPropertyChanged([CallerMemberName] string name = "") {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
 
         public ICommand RemovePassCommand { get; private set; }
         public ICommand FetchPassCommand { get; private set; }
 
         public SeasonPassViewModel() {
 
-            LoadSavedPass();
-
-            RemovePassCommand = new Command(RemovePassAction);
-            FetchPassCommand = new Command(FetchPassAction);
-        }
-
-        public string SeasonPassInput {
-            get { return _SeasonPassInput; }
-            set {
-                _SeasonPassInput = value;
-                OnPropertyChanged();
-            }
-        }
-        public bool IsBusy {
-            get { return _IsBusy; }
-            set { _IsBusy = value; }
-        }
-        public bool HasValidPass {
-            get { return _HasValidPass; }
-            set { _HasValidPass = value; }
-        }
-        public bool IsWorking {
-            get { return _IsWorking; }
-            set { _IsWorking = value; }
-        }
-        public SeasonPassModel ValidPassData {
-            get { return _ValidPassData; }
-            set { _ValidPassData = value;  }
-        }
-        private void FetchPassAction() {
-
-            bool Valid = false;
-            
-            if(SeasonPassInput == "") {
-                return;
-            }
-
-            IsBusy = true;
-            OnPropertyChanged(nameof(IsBusy));
-           
-            List<RequestHeader> RequestHeaders = new List<RequestHeader>();
-            RequestHeaders.Add(new RequestHeader() {
-                Key = "PassID",
-                Value = EncDecService.Hash(SeasonPassInput)
+            Task.Run(() => {
+                Service.Get();
+                IsBusy = false;
+                Refresh();
             });
-            ApiResourceController ApiResource = new ApiResourceController("/seasonpass/get", RequestHeaders);
 
-            using(var Response = ApiResource.Get()) {
-                if(Response != null) {
-                    string Data = Response.Result;
-                    List<SeasonPassModel> DataParsed = JsonConvert.DeserializeObject<List<SeasonPassModel>>(Data);
-                    if(DataParsed.Count > 0) {
-                        SeasonPassModel LoadedPass = DataParsed[0];
-                        if(LoadedPass.IsValid()) {
-                            CrossSettings.Current.AddOrUpdateValue(_SeasonPassDataKey, JsonConvert.SerializeObject(LoadedPass));
-                            LoadedPass.Holder = EncDecService.Decrypt(LoadedPass.Holder);
-                            ValidPassData = LoadedPass;
-                            OnPropertyChanged(nameof(ValidPassData));
-                            Valid = true;
-                        } else {
-                            PopupController.Message("Verification error", "The pass does not seem to be valid. Please check the information entered and try again.", "Dismiss");
-                            Valid = false;
-                        }
-                    } else {
-                        PopupController.Message("Verification error", "The pass does not seem to be valid. Please check the information entered and try again.", "Dismiss");
-                        Valid = false;
-                    }
-                } else {
-                    Valid = false;
-                }
-            }
-            
+            RemovePassCommand = new Command(() => {
+                Task.Run(() => {
+                    Service.Remove();
+                    Refresh();    
+                });
+            });
+            FetchPassCommand = new Command(() => {
+                Task.Run(() => {
 
-            HasValidPass = Valid;
-            OnPropertyChanged(nameof(HasValidPass));
+                    IsBusy = true;
+                    Refresh();
 
-            IsBusy = false;
-            OnPropertyChanged(nameof(IsBusy));
+                    Service.Get(UserInputValue);
 
-            return;
+                    IsBusy = false;
+                    Refresh();
 
+                });
+            });
         }
-        private void LoadSavedPass() {
-
-            IsBusy = true;
-            OnPropertyChanged(nameof(IsBusy));
-
-            bool Valid = false;
-
-            string MemoryData = CrossSettings.Current.GetValueOrDefault(_SeasonPassDataKey, null);
-
-            if(MemoryData == null) {
-                Valid = false;
-            } else {
-                
-                SeasonPassModel Deserialized = JsonConvert.DeserializeObject<SeasonPassModel>(MemoryData);
-                Debug.WriteLine(Deserialized);
-
-                if(Deserialized.IsValid()) {
-                    Deserialized.Holder = EncDecService.Decrypt(Deserialized.Holder);
-                    ValidPassData = Deserialized;
-                    OnPropertyChanged(nameof(ValidPassData));
-                    Valid = true;
-                } else {
-                    Valid = false;
-                }
-            }
-
-            HasValidPass = Valid;
-            OnPropertyChanged(nameof(HasValidPass));
-
-            IsBusy = false;
-            OnPropertyChanged(nameof(IsBusy));
-
+        void OnPropertyChanged([CallerMemberName] string name = "") {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
-        private void RemovePassAction() {
-
-            IsBusy = true;
-            OnPropertyChanged(nameof(IsBusy));
-
-            CrossSettings.Current.Remove(_SeasonPassDataKey);
-
-            HasValidPass = false;
-            OnPropertyChanged(nameof(HasValidPass));
-
-            IsBusy = false;
-            OnPropertyChanged(nameof(IsBusy));
-
+        private void Refresh() {
+            OnPropertyChanged("Pass");
+            OnPropertyChanged("IsBusy");
+            OnPropertyChanged("ShowPassDetails");
+            OnPropertyChanged("ShowPassInput");
         }
     }
 }
