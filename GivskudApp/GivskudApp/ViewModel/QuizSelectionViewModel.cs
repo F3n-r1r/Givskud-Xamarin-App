7c132ac4ffb48372d6d1ec44d662198d776ddeb0
jiri-kralovec-eal <jiri0042@edu.eal.dk>
2019-05-07 19:10:32 +0200

    [Finished] Quiz selection page

---
+++ GivskudApp/GivskudApp/ViewModel/QuizSelectionViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+using GivskudApp.Services;
+using GivskudApp.Models;
+
+namespace GivskudApp.ViewModel
+{
+    class QuizSelectionViewModel : INotifyPropertyChanged
+    {
+        public bool _IsBusy = true;
+        public bool IsBusy { get { return _IsBusy; } set { _IsBusy = value; } }
+        public bool IsLoaded { get { return !IsBusy; } set { _IsBusy = !value; } }
+
+        private QuizService Service = new QuizService();
+        public List<QuizModel> Data { get { return Service.Data; } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public QuizSelectionViewModel()
+        {
+            Task.Run(() =>
+            {
+
+                Service.Get();
+                IsBusy = false;
+                Refresh();
+
+            });
+        }
+
+        public void Refresh()
+        {
+            OnPropertyChanged("Data");
+            OnPropertyChanged("IsBusy");
+            OnPropertyChanged("IsLoaded");
+        }
+    }
+}
--- GivskudApp/GivskudApp/Views/QuizSelectionPage.xaml.cs
+++ GivskudApp/GivskudApp/Views/QuizSelectionPage.xaml.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using DLToolkit.Forms.Controls;
+
 using GivskudApp.Models;
 using GivskudApp.Controllers;
+using GivskudApp.ViewModel;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,68 +18,21 @@ namespace GivskudApp.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class QuizSelectionPage : ContentPage
 	{
+
+        private QuizSelectionViewModel vm { get; set; }
+
 		public QuizSelectionPage ()
 		{
+
+            vm = new QuizSelectionViewModel();
+
 			InitializeComponent ();
             GuiInstanceController.AnnaGuiInstance AnnaOverlay = new GuiInstanceController.AnnaGuiInstance(ApplicationLayoutTopLevel);
-            QuizList.FlowItemsSource = new List<QuizModel> {
-                new QuizModel {
-                    ID = 1234,
-                    Title = "Quiz 1",
-                    Image = "https://www.sciencemag.org/sites/default/files/styles/inline__450w__no_aspect/public/NationalGeographic_1561927_16x9.jpg?itok=q7LvZb-6",
-                    IsLockedByDefault = false,
-                    Questions = new List<QuizQuestionModel> {
-                        new QuizQuestionModel {
-                            Question = "Question 1",
-                            Answers = new Dictionary<int, string> {
-                                {1, "Answer 1 Q1" },
-                                {2, "Answer 2 Q1" },
-                                {3, "Answer 3 Q1" },
-                                {4, "Answer 4 Q1" }
-                            },
-                            CorrectAnswer = 2
-                        },
-                        new QuizQuestionModel {
-                            Question = "Question 2",
-                            Answers = new Dictionary<int, string> {
-                                {1, "Answer 1 Q2" },
-                                {2, "Answer 2 Q2" },
-                                {3, "Answer 3 Q2" },
-                                {4, "Answer 4 Q2" }
-                            },
-                            CorrectAnswer = 1
-                        }
-                    }
-                },
-                new QuizModel {
-                    ID = 1234,
-                    Title = "Quiz 2",
-                    Image = "https://www.sciencemag.org/sites/default/files/styles/inline__450w__no_aspect/public/NationalGeographic_1561927_16x9.jpg?itok=q7LvZb-6",
-                    IsLockedByDefault = false,
-                    Questions = new List<QuizQuestionModel> {
-                        new QuizQuestionModel {
-                            Question = "Question 1",
-                            Answers = new Dictionary<int, string> {
-                                {1, "Answer 1 Q1" },
-                                {2, "Answer 2 Q1" },
-                                {3, "Answer 3 Q1" },
-                                {4, "Answer 4 Q1" }
-                            },
-                            CorrectAnswer = 2
-                        },
-                        new QuizQuestionModel {
-                            Question = "Question 2",
-                            Answers = new Dictionary<int, string> {
-                                {1, "Answer 1 Q2" },
-                                {2, "Answer 2 Q2" },
-                                {3, "Answer 3 Q2" },
-                                {4, "Answer 4 Q2" }
-                            },
-                            CorrectAnswer = 1
-                        }
-                    }
-                }
-            };
+            ElementsController.RenderScannerIcon(ApplicationLayoutTopLevel, Navigation);
+
+            FlowListView.Init();
+
+            BindingContext = vm;
         }
         public async void ItemClicked(object sender, ItemTappedEventArgs e)
         {
